7b7389d8e59e7d6f861460095c5a6bb49545ced7
timespliced <fred@laptop>
2020-07-04 16:37:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Why
+
+Hypothes.is is a great service:
+
+* It works well,
+* It is free,
+* It has been around forever.
+
+but there are drawbacks:
+
+* It is not intended for mobile use,
+* I always need my last pages annotated handy.
+
+# What
+
+Share via hypothesis a progressive web app to improve the experience of the hypothes.is annotation service on mobile.
+
+On mobile it allows to "share" an URL to annotate it.
+It keep an local history of the last 10 article annotated so you have them handy.
+
+# Where
+
+A public instance run on now.sh 
+
+If you want to customize it you can easly host your own with Vercel or Surge
+
+# How
+
+1. Go to xxx.now.sh
+2. Add to home screen
+3. Open a web page, click share & select "Via Hypothesis"
+
+TODO add a screenshot
+
+The app leverage a few others technologies & services:
+
+* [PWA](https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps): leverage [A2HS](https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen) & [Web Share Target](https://web.dev/web-share-target/) API
+* [via.hypothes.is](https://via.hypothes.is)
+* [Preact](https://preactjs.com/): only JS dependency
+* MinDS
+
+# Who
+
+By an hypothes.is user for hypothes.is users 
---
+++ src/index.js
@@ -0,0 +1,88 @@
+import './style';
+import { useState, useEffect } from 'preact/hooks';
+
+const viaUrl = 'https://via.hypothes.is/';
+const historySize = 10;
+
+function App() {
+	const [formUrl, setFormUrl] = useState('');
+	const [lastUrls, setLastUrls] = useState([]);
+
+	useEffect(() => {
+		let urlsLocalString = localStorage.getItem('lastsViaHyp');
+
+		if (urlsLocalString) setLastUrls(JSON.parse(urlsLocalString));
+
+		// Shared data is send as URL search params.
+		let parsedUrl = new URL(window.location.href);
+		let text = parsedUrl.searchParams.get("text");
+		let url = parsedUrl.searchParams.get("url");
+
+		if (url) {
+			setFormUrl(url);
+		} else if (text) {
+			setFormUrl(text);
+		}
+
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (formUrl != '') {
+			manageLocalHistory(formUrl)
+			//document.location.href = (viaUrl + formUrl);
+			window.open(viaUrl + formUrl);
+		}
+	}
+
+	const manageLocalHistory = (url) => {
+		let r = lastUrls;
+		let already = lastUrls.indexOf(url);
+
+		if (already >= 0) r.splice(already, 1);
+		r.unshift(url);
+		if (r.length > historySize) r.pop();
+
+		localStorage.setItem('lastsViaHyp', JSON.stringify(r))
+	}
+
+	return (
+		<>
+			<header><span class="logo">h.</span> Share via <a href="https://hypothes.is/">hypothes.is</a></header>
+			<form 
+				class="url-form"
+				onSubmit={handleSubmit}
+				novalidate>
+				<input id="search"
+					aria-label="Web or PDF document to annotate"
+					autofocus
+					placeholder="Paste a link to create or view annotations"
+					type="url"
+					value={formUrl}
+					onChange={e => setFormUrl(e.target.value)}
+				/>
+				<button type="submit">Annotate</button>
+			</form>
+			{lastUrls.length > 0 &&
+			<article>
+				<header>Local history</header>
+				<ul>
+				{lastUrls.map(x => new URL(x))
+								 .map(x => <li key={x.href}>
+														 <img 
+															 style={{width: 16, float:'left', marginRight: 5}} 
+															 src={x.origin + '/favicon.ico'} 
+															 onError={(e)=>{e.target.onerror = null; e.target.src='/assets/icons/favicon.default.png'}}
+															 />
+									 					 <a class="overflow" href={viaUrl + x.href}>
+															 {x.host + x.pathname}
+														</a>
+													 </li>)}
+				</ul>
+			</article>}
+			<footer><span>This app is not affiliated with the official hypothesis service</span> <a href="">Sources & doc</a></footer>
+		</>
+	);
+}
+
+export default App;
